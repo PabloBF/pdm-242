842883e6dd84ad0f4b7a0b0f58c9d4ee40e995ef
Pablo Busatto <55034604+PabloBF@users.noreply.github.com>
2025-02-18 18:42:49 -0300

    Atividade 7

--- atividades/atividade-07/test/widget_test.dart
+++ atividades/atividade-07/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:minha_loja/main.dart';
+import 'package:api_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
